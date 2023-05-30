448ff09872ea857f2c317e679f6461813c1b949e
Nam Nguyen <ducnam1719@gmail.com>
2023-05-30 08:45:57 +0700

    add slide swiper

--- components/home/main/index.js
+++ components/home/main/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import styles from './styles.module.scss'
+import MainSwiper from './swiper'
 
 export default function index() {
   return (
     <div className={styles.main}>
       <div className={styles.header}>Header</div>
       <div className={styles.menu}>Menu</div>
-      <div className={styles.swiper}>Swiper</div>
+      <MainSwiper />
       <div className={styles.offers}>Offer</div>
       <div className={styles.user}>User</div>
     </div>
